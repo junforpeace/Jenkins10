c795b15e9953a38bdd46787b09781e7301879879
junforpeace <soapcream@gmail.com>
2022-05-30 20:45:09 +0200

    нравится коммитить

--- src/test/java/tests/simple/ForAttachs.java
+++ src/test/java/tests/simple/ForAttachs.java
@@ -3,7 +3,6 @@ package tests.simple;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
-import io.qameta.allure.model.Attachment;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
